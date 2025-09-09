7dc9db80d612d5b210320d7f8a982b5ef3cbe912
Sabring Song <150405848+HEALOON@users.noreply.github.com>
2025-09-09 13:27:27 +0800

    Update sketch.js

--- abc-multi-touch/sketch.js
+++ abc-multi-touch/sketch.js
@@ -4,28 +4,45 @@ function setup() {
 }
 
 function draw() {
-  // background(90, 200, 190);
-  
+  background(90, 200, 190);
+
+  // 四个区块
+  let blockH = height / 4;
+  let colors = [
+    color(240, 248, 255),
+    color(220, 230, 240),
+    color(200, 210, 220),
+    color(180, 190, 200)
+  ];
+  noStroke();
+  for (let i = 0; i < 4; i++) {
+    fill(colors[i]);
+    rect(0, i * blockH, width, blockH);
+  }
+
+  // 根据触摸情况，决定哪个区块显示圆
+  for (let i = 0; i < 4; i++) {
+    if (isBlockTouched(i, blockH)) {
+      fill(0);
+      circle(width - 50, i * blockH + blockH / 2, 50);
+    }
+  }
+
+  // 保留：中央黑色圆
   fill(0);
-  circle(width/2, height/2, 100);
-
+  circle(width / 2, height / 2, 100);
 }
 
-// P5 touch events: https://p5js.org/reference/#Touch
-
-function touchStarted() {
-  console.log(touches);
-  background(200,0,0);
+// 判断某个区块是否有触摸
+function isBlockTouched(index, blockH) {
+  for (let t of touches) {
+    if (t.y > index * blockH && t.y < (index + 1) * blockH) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function touchMoved() {
-}
-
-function touchEnded() {
-  background(255);
-}
-
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
